63eae7f7332c7cf886f7e2279e8b211ba5267e56
Marko Kofol <mk8394@student.uni-lj.si>
2021-11-26 22:45:49 +0100

    Weapon pickup and animation.

---
+++ 17-game/Weapon.js
@@ -0,0 +1,24 @@
+import { Node } from './Node.js';
+
+export class Weapon extends Node {
+
+    constructor(mesh, image, options) {
+        super(options);
+        this.mesh = mesh;
+        this.image = image;
+    }
+
+    animate() {
+        let frame = 0;
+        console.log(this);
+        const anim = setInterval(() => {
+            this.rotation[1] += 1;
+            this.updateTransform();
+            frame += 1;
+            console.log(frame)
+            if(frame >= 60)
+                clearInterval(anim);
+        }, 1);
+    }
+
+}
